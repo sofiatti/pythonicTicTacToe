6b04336a60bc69bb5bb2488b794db209e4a03b90
Caroline Sofiatti <c.sofiatti@gmail.com>
2015-06-11 10:51:32 -0400

    First attempt at package

--- pythonicTTT/pythonicTTT/TicTacToe.py
+++ pythonicTTT/pythonicTTT/TicTacToe.py
@@ -1,4 +1,4 @@
-class game():
+class Game():
     def __init__(self):
         self.board = [' '] * 10
         self.corners = [1,3,7,9]
@@ -15,15 +15,16 @@ class game():
         print ' ' + self.board[7] + '|' + self.board[8] + '|' + self.board[9] + '     7|8|9'
         print '\n'
 
-    def check_if_won(self):
+    @classmethod
+    def check_if_won(cls, board):
         for p in ["X", "O"]:
             possibilities = [
                 #Consecutive line
-                self.board[1:4], self.board[4:7], self.board[7:10],
+                board[1:4], board[4:7], board[7:10],
                 #Consecutive Row:
-                self.board[1::3], self.board[2::3], self.board[3::3],
+                board[1::3], board[2::3], board[3::3],
                 #Consecutive Diagonals:
-                self.board[1::4], self.board[3::2]
+                board[1::4], board[3::2]
                 ]
 
             for element in possibilities:
@@ -31,39 +32,39 @@ class game():
                     return True
         return False
 
-def get_play(symbol, board):
-    instructions = "You are " + symbol + ". Please enter your play [1-9]: "
-    try: 
-        play = int(raw_input(instructions))
-    except ValueError:
-        print "Please enter a number from 1 to 9."  
-        return get_play(symbol, board)
-    try:
-        if play < 0 and play > 9:
-            raise Exception('Only 1 through 9 are valid.')
-    except Exception, e:
-        print str(e)
-        return get_play(symbol, board) 
-    while board[play] != ' ':
-        print 'Oops!  That\'s taken!  Try again...'
-        return get_play(symbol, board)
-    return play  
+    def get_play(self, symbol):
+        instructions = "You are " + symbol + ". Please enter your play [1-9]: "
+        try: 
+            play = int(raw_input(instructions))
+        except ValueError:
+            print "Please enter a number from 1 to 9."  
+            return self.get_play(symbol)
+        try:
+            play < 1 and play > 9
+            raise Exception('**Error** Only numbers 1 through 9 are valid.\n')
+        except Exception, e:
+            print str(e)
+            return self.get_play(symbol) 
+        while self.board[play] != ' ':
+            print 'Oops!  That\'s taken!  Try again...'
+            return self.get_play(symbol)
+        return play  
 
-def get_value_bot_or_friend():
-    bot_or_friend = raw_input("Who would you like to play? (bot/friend) ")
-    try:
-        if bot_or_friend not in ['bot', 'friend']:
-            raise Exception("Only 'bot' or 'friend' are valid inputs.")
-        else:
-            return bot_or_friend
-    except Exception, e:
-        str(e)
-        return get_value_bot_or_friend()    
+    def get_value_bot_or_friend():
+        bot_or_friend = raw_input("Who would you like to play? (bot/friend) ")
+        try:
+            if bot_or_friend not in ['bot', 'friend']:
+                raise Exception("Only 'bot' or 'friend' are valid inputs.")
+            else:
+                return bot_or_friend
+        except Exception, e:
+            str(e)
+            return get_value_bot_or_friend()    
 
-    def next_step(idx, current_player, self):
+    def next_step(self, idx, current_player):
         hypothetical_board = list(self.board)
         hypothetical_board[idx] = current_player
-        if check_if_won(hypothetical_board):
+        if Game.check_if_won(hypothetical_board):
             return idx
 
     def empty_spaces(self):
@@ -74,60 +75,60 @@ def get_value_bot_or_friend():
                 empty_idx.append(idx)
         return empty_idx
 
-    def get_bot_move(current_player, self):
-        possible_plays = empty_spaces(self.board)
+    def get_bot_move(self, current_player):
+        possible_plays = self.empty_spaces()
         for next_play in possible_plays:
             # check if bot can win in the next move
-            play = next_step(next_play, current_player, self.board)
+            play = self.next_step(next_play, current_player)
             if play:
                 return play
             # check player can win in the next move
-            opponent = other_player(str(current_player))
-            play = next_step(next_play, opponent, self.board)
+            opponent = self.other_player(str(current_player))
+            play = self.next_step(next_play, opponent)
             if play:
                 return play
         # check if space in corners, take it
-        for play in corners:
+        for play in self.corners:
             if play in possible_plays:
                 return play
-        for play in middle:
+        for play in self.middle:
             if play in possible_plays:
                 return play
-        for play in sides:
+        for play in self.sides:
             if play in possible_plays:
                 return play
-
-    def other_player(current_player):
+    @classmethod
+    def other_player(cls, current_player):
         if current_player == "X":
             return "O"
         elif current_player == "O":
             return "X"
 
     def make_a_play(self, current_player):
-        draw(self.board)
-        p = check_if_won(self.board)
+        self.draw()
+        p = Game.check_if_won(self.board)
         
     def start_game(self):
         print 'Welcome to Tic-Tac-Toe! '
         bot_or_friend = raw_input("Who would you like to play? (bot/friend) ")
-        self.draw(self)
+        self.draw()
         current_player = "X"
         while ' ' in self.board[1:]:
             if bot_or_friend == 'bot':
-                self.board[get_bot_move(current_player, self.board)] = current_player
-                self.draw(self)
-                if self.check_if_won(self.board):
+                bot_move = self.get_bot_move(current_player)
+                self.board[bot_move] = current_player
+                self.draw()
+                if Game.check_if_won(self.board):
                     print 'Game Over! %s won!!!' %current_player
-                current_player = other_player(current_player)
+                current_player = Game.other_player(current_player)
                 if ' ' not in self.board[1:]:
                     break
-            self.board[get_move(current_player, self)] = current_player
-            if self.check_if_won(self):
+            self.board[self.get_play(current_player)] = current_player
+            if Game.check_if_won(self.board):
                 print 'Game Over! %s won!!!' %current_player
-            current_player = other_player(current_player)
+            current_player = Game.other_player(current_player)
         print 'Whomp whomp, it\'s a tie!'
 
 if __name__ == "__main__":
-    tic_tac_toe = game()
+    tic_tac_toe = Game()
     tic_tac_toe.start_game()
-    tic_tac_toe()
---
+++ pythonicTTT/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='pythonicTTT',
+      version='0.1',
+      description='A pythonista\'s Tic-Tac-Toe',
+      url='http://github.com/sofiatti/pythonicTicTacToe',
+      author='Caroline Sofiatti and Luna Lunapiena',
+      author_email='c.sofiatti@gmail.com',
+      license='MIT',
+      packages=['pythonicTTT'],
+      zip_safe=False)
